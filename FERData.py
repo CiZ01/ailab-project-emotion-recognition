ae3e483ee5095392a1f92ac31cdef6744e19c644
CiZ01 <ffrancy01@gmail.com>
2025-02-20 12:49:48 +0100

    reformat repo and updated

--- FERData.py
+++ FERData.py
@@ -13,24 +13,34 @@ class FERDataset(Dataset):
         trasform: torchvision.transforms
 
         '''
-        assert dataset_path.split('/')[-1] in {'train', 'test'}, 'dataset_path should be the parent directory of train and test'
+        assert dataset_path.split('/')[-1] in {'train', 'test', 'val'}, 'dataset_path should be the parent directory of train and test'
         
         self.image_paths = glob.glob(f'{dataset_path}/*/*.jpg')
-        self.labels = [p.split('/')[-1] for p in glob.glob(f'{dataset_path}/*')]     
+        self.labels = {k.split('/')[-1]:v for (v,k) in enumerate(glob.glob(f'{dataset_path}/*'))}
         self.transform = transform
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, index):
-        image = np.asarray(Image.open(self.image_paths[index]))
-        label = self.image_paths[index].split('/')[-2]
+        if isinstance(index, tuple):
+            sub_labels = index[-1]
+            return [(np.asarray(Image.open(img)) / 255.0, self.labels[img.split('/')[-2]]) for img in self.image_paths[index[0]] if img.split('/')[-2] in sub_labels]
+        
+        if isinstance(index, slice):
+            return [(np.asarray(Image.open(img)) / 255.0, self.labels[img.split('/')[-2]]) for img in self.image_paths[index]]
+        
+
+        # Open image and convert to numpy array, then normalize to [0, 1].
+        image = Image.open(self.image_paths[index])
+        label = self.labels[self.image_paths[index].split('/')[-2]]
         
         if self.transform is not None:
             image = self.transform(image)
 
         return image, label
-    
+
 if __name__ == '__main__':
-    dataset = FERDataset('./dataset/train')
-    print(dataset[0])
+    dataset = FERDataset('./dataset/test')
+    print(len(dataset))
+    print(np.array(dataset[0][0]).shape)
